75e279e5d85172e133b6327b00271ba563c381cf
Nikhil Arora <narora200@gmail.com>
2018-08-07 14:02:38 +0530

    Revert "[Feat]: Game working but without Timer"

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
 import React, { Component } from 'react';
-import { Layout } from 'antd';
 
-import Game from './components/Game';
-
-
-const { Header, Footer, Content } = Layout;
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Layout
-          style={{
-            minHeight: '100vh',
-          }}
-        >
-          <Header
-            style={{
-              color: '#fff',
-              textAlign: 'center',
-              fontSize: '2em',
-            }}
-          >
-            Speed Typing
-          </Header>
-          <Content>
-            <Game />
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>
-            Made with <span role="img" aria-label="heart">❤️</span> by Nikhil
-          </Footer>
-        </Layout>
+        <header>
+          <h1>
+            Welcome to React
+          </h1>
+        </header>
+        <p>
+          To get started, edit
+          <code>
+            src/App.js
+          </code>
+          and save to reload.
+        </p>
       </div>
     );
   }
